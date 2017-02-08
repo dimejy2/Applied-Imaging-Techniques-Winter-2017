796541db732a1b74116ff78644275279abf5ef3d
Dimeji Abidoye <dimejy2@gmail.com>
2017-02-08 10:25:58 +0000

    updates to q4

--- tut2/q4.m
+++ tut2/q4.m
@@ -6,24 +6,23 @@ for k = 1 : length(files)
     name = char(files(k));
     info = imfinfo(name);
     bitDepth = info.BitDepth;
-    
+
     if bitDepth == 8
         reduceRange(name,7, 1);
-    end 
-end 
-    
+    end
+end
+
 function reduceRange(name, start, en)
     f= imread(name);
     figure
     subplot(start - en + 1, 1, 1), imshow(f);
-    
-    disp('here')
+
     for k = start : -1 : en
         reduced = reduceImage(f, 8, k);
         subplot(start -en + 1, 1 , start-k+1), imshow(reduced);
-    end 
-    
-    
+    end
+
+
  function reduced = reduceImage(f, base, k)
      reduced = floor(f.*(2^(k - base)));
 
